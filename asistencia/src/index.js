bc7119e63449845695b280a2c22af860d45ab1f9
Felipe <felipe.ortiz.san@gmail.com>
2022-05-01 17:50:33 -0400

    Quité el renderizado de la pantalla crearModulo en el index

--- asistencia/src/index.js
+++ asistencia/src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import CrearModulo from './Paginas/CrearModulo'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <CrearModulo/>
   </React.StrictMode>
 );
