e47d66bcb1e60a11bf3f84d540b1a3c32293087d
EduVale581 <46286246+EduVale581@users.noreply.github.com>
2022-05-17 00:39:25 -0400

    Finalizacion modificar perfil
    Se ha finalizado la modificacion de perfil

--- asistencia/src/Paginas/Perfil.js
+++ asistencia/src/Paginas/Perfil.js
@@ -3,52 +3,82 @@ import NavbarInicio from '../Componentes/NavbarInicio';
 import { useAuth } from '../Context/AuthContext';
 import {
     Stack,
-    TextField,
-    Typography
+    Alert,
+    Typography,
+    FormControl,
+    InputLabel,
+    OutlinedInput,
+    IconButton,
+    InputAdornment
 } from '@mui/material/';
 import { useNavigate } from "react-router-dom"
 import VisualizarModuloEstudiante from './VisualizarModuloEstudiante';
-import { db } from '../Utils/firebase';
+import { auth } from '../Utils/firebase';
 import { collection, query, where, getDocs } from "firebase/firestore";
 import { semestreActual } from '../Utils/funciones';
 
+import { updatePassword } from "firebase/auth";
+
 import AddIcon from "@mui/icons-material/Add";
 import { LoadingButton } from '@mui/lab';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 
 export default function Perfil() {
     const { currentUser } = useAuth();
-    console.log(currentUser)
 
-    const [nombre, setNombre] = useState("")
-    const [numeroMatricula, setNumeroMatricula] = useState("")
+    const [contrasena, setContrasena] = useState("")
+    const [verificacionContrasena, setVerificacionContrasena] = useState("")
+    const [mostrarContrasena, setMostrarContrasena] = useState(false)
+    const [cargando, setCargando] = useState(false);
+    const [alerta, setAlerta] = useState(false);
+    const [colorAlerta, setColorAlerta] = useState("success");
+    const [mensajeAlerta, setMensajeAlerta] = useState("");
 
-    useEffect(() => {
-        if (currentUser.tipoUsuario !== "Profesor") {
-            const q = query(collection(db, "estudiantes"), where("correo", "==", currentUser.email));
-            getDocs(q).then((querySnapshot) => {
-                querySnapshot.forEach((doc) => {
-                    // doc.data() is never undefined for query doc snapshots
-                    console.log(doc.id, " => ", doc.data());
-                    if (doc.data().nombre) {
-                        setNombre(doc.data().nombre)
+    const handleChangeContrasena = (e) => {
+        setContrasena(e.target.value)
+    }
 
-                    }
-                    if (doc.data().numeroMatricula) {
-                        setNumeroMatricula(doc.data().numeroMatricula)
+    const handleChangeVerificacionContrasena = (e) => {
+        setVerificacionContrasena(e.target.value)
+    }
 
-                    }
-                });
+    const handleClickShowPassword = () => {
+        setMostrarContrasena(!mostrarContrasena);
+    };
 
-            }).catch(() => {
+    const guardarDatos = () => {
+        setCargando(true)
+        setMensajeAlerta("");
+        setColorAlerta("success");
+        setAlerta(false);
+        if (contrasena === verificacionContrasena) {
+            const user = auth.currentUser;
+            console.log(user)
 
-            })
+            updatePassword(user, contrasena).then(() => {
+                setMensajeAlerta("Contraseña actualizada con éxito");
+                setColorAlerta("success");
+                setCargando(false)
+                setTimeout(setAlerta(true), 3000);
+            }).catch((error) => {
+                setMensajeAlerta("Ha ocurrido un error al actualizar la contraseña");
+                setColorAlerta("error");
+                setAlerta(true);
+                setCargando(false)
+            });
 
-
+        }
+        else {
+            setMensajeAlerta("Las contraseñas no coinciden");
+            setColorAlerta("error");
+            setAlerta(true);
+            setCargando(true)
 
         }
 
-    }, [currentUser.tipoUsuario, currentUser.email])
+    };
 
 
 
@@ -58,42 +88,60 @@ export default function Perfil() {
 
             <Stack spacing={2} style={{ marginTop: "50px", marginRight: "50px", marginLeft: "50px" }}>
                 <Typography variant="h4" style={{ textAlign: "center" }} color="primary">{currentUser.email}</Typography>
-                {currentUser.tipoUsuario !== "Profesor" && (
-                    <Stack spacing={2}>
-                        <TextField
-                            id="outlined-basic"
-                            label="Número Matricula"
-                            variant="outlined"
-                            value={numeroMatricula}
+                <FormControl variant="outlined">
+                    <InputLabel htmlFor="outlined-adornment-password">Nueva Contraseña</InputLabel>
+                    <OutlinedInput
+                        id="outlined-adornment-password"
+                        type={mostrarContrasena ? 'text' : 'password'}
+                        value={contrasena}
+                        onChange={handleChangeContrasena}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    onClick={handleClickShowPassword}
+                                    edge="end"
+                                >
+                                    {mostrarContrasena ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                        label="Nueva Contraseña"
+                    />
+                </FormControl>
 
-                        />
-                        <TextField
-                            id="outlined-basic"
-                            label="Nombre"
-                            variant="outlined"
-                            value={nombre}
-                        />
+                <FormControl variant="outlined">
+                    <InputLabel htmlFor="outlined-adornment-password2">Confirmación Nueva Contraseñaa</InputLabel>
+                    <OutlinedInput
+                        id="outlined-adornment-password2"
+                        type={mostrarContrasena ? 'text' : 'password'}
+                        value={verificacionContrasena}
+                        onChange={handleChangeVerificacionContrasena}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    onClick={handleClickShowPassword}
+                                    edge="end"
+                                >
+                                    {mostrarContrasena ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                        label="Confirmación Nueva Contraseña"
+                    />
+                </FormControl>
 
+                {
+                    alerta && (
+                        <Alert severity={colorAlerta}>{mensajeAlerta}</Alert>
 
+                    )
 
-                    </Stack>
-                )}
-                <TextField
-                    id="outlined-basic"
-                    label="Nueva Contraseña"
-                    variant="outlined"
-                    type={"password"}
-                />
-
-                <TextField
-                    id="outlined-basic"
-                    label="Confirmación Nueva Contraseña"
-                    variant="outlined"
-                    type={"password"}
-                />
+                }
 
                 <LoadingButton
+                    loading={cargando}
                     variant='contained'
+                    onClick={guardarDatos}
                 >
                     Guardar Datos
                 </LoadingButton>
