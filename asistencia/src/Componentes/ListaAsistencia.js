c638de874cc76fcf0b1e3d0b7bf2fde8a3ac60f6
EduVale581 <46286246+EduVale581@users.noreply.github.com>
2022-05-19 23:52:02 -0400

    Modificacion interfaz toma asistencia
    Se ha modificado la interfaz de toma de asistencia y se obtienen los datos en tiempo real.

--- asistencia/src/Componentes/ListaAsistencia.js
+++ asistencia/src/Componentes/ListaAsistencia.js
@@ -1,45 +1,41 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import {
     Button,
     Grid,
-    ListItem,
-    ListItemIcon,
-    ListItemText,
-    Stack,
 } from '@mui/material';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { db } from '../Utils/firebase'
-import { collection, doc, addDoc, updateDoc,query , where, getDocs } from "firebase/firestore";
+import { collection, doc, addDoc, updateDoc, query, where, getDocs } from "firebase/firestore";
+import TablaAsistenciaModulo from './TablaAsistenciaModulo';
 
 
-function fechaHoy(){
+function fechaHoy() {
     var hoy = new Date();
     let diaSemana = '';
 
-    if(hoy.getDay()===1){
+    if (hoy.getDay() === 1) {
         diaSemana = 'Lunes'
     }
-    if(hoy.getDay()===2){
+    if (hoy.getDay() === 2) {
         diaSemana = 'Martes'
     }
-    if(hoy.getDay()===3){
+    if (hoy.getDay() === 3) {
         diaSemana = 'Miércoles'
     }
-    if(hoy.getDay()===4){
+    if (hoy.getDay() === 4) {
         diaSemana = 'Jueves'
     }
-    if(hoy.getDay()===5){
+    if (hoy.getDay() === 5) {
         diaSemana = 'Viernes'
     }
-    if(hoy.getDay()===6){
+    if (hoy.getDay() === 6) {
         diaSemana = 'Sábado'
     }
-    if(hoy.getDay()===0){
+    if (hoy.getDay() === 0) {
         diaSemana = 'Domingo'
     }
-    return{
+    return {
         dia: hoy.getDate(),
-        mes: hoy.getMonth()+1,
+        mes: hoy.getMonth() + 1,
         anio: hoy.getFullYear(),
         diaSemana: diaSemana,
         hora: hoy.getHours(),
@@ -47,115 +43,113 @@ function fechaHoy(){
         segundos: hoy.getSeconds(),
     }
 
-    
+
 }
 
 
-const ListaAsistencia = ({estudiantes, modulo, id}) => {
+const ListaAsistencia = ({ estudiantes, modulo, id }) => {
 
     const [iniciarAsistencia, setIniciarAsistencia] = useState(false);
-    const [fechaActual, setFechaActual] = useState(fechaHoy());
-    const [estado, setEstado] = useState(false); 
-    const [bloqueActual, setBloqueActual]= useState("");
-    const [idNueva,setIDNueva] = useState("");
-    const [asisNueva, setAsisNueva] = useState(null);
-    console.log("Bloque actual: "+bloqueActual);
-   
-    const tomarAsistencia = () =>{
+    const fechaActual = fechaHoy();
+    const [estado, setEstado] = useState(false);
+    const [bloqueActual, setBloqueActual] = useState("");
+    console.log("Bloque actual: " + bloqueActual);
+
+    const tomarAsistencia = () => {
         const hoy = fechaActual.diaSemana;
         const hora = fechaActual.hora;
         const minutos = fechaActual.minutos;
-       
- 
-        
-        modulo.horario.map((horario) =>{
+
+
+
+        modulo.horario.forEach((horario) => {
             /* console.log({hoy})
             console.log(bloque.diaSemana); */
-            if(horario.diaSemana === hoy && estado!= true){
-                console.log('Es el dia correcto: '+hoy)
-                console.log({hora, minutos});
-                if(horario.bloque === 'Bloque 1'){
-                    if((hora >=8 && minutos >= 30) || (hora <=9 && minutos <= 30) ){
+            if (horario.diaSemana === hoy && estado !== true) {
+                console.log('Es el dia correcto: ' + hoy)
+                console.log({ hora, minutos });
+                if (horario.bloque === 'Bloque 1') {
+                    if ((hora >= 8 && minutos >= 30) || (hora <= 9 && minutos <= 30)) {
                         console.log('Bloque 1')
                         setBloqueActual("Bloque 1")
                         setEstado(true);
                     }
                 }
-                if(horario.bloque === 'Bloque 2'){
-                    if((hora >=9 && minutos >= 40) || (hora <=10 && minutos <= 40) ){
+                if (horario.bloque === 'Bloque 2') {
+                    if ((hora >= 9 && minutos >= 40) || (hora <= 10 && minutos <= 40)) {
                         console.log('Bloque 2')
                         setBloqueActual("Bloque 2")
                         setEstado(true);
                     }
                 }
-                if(horario.bloque === 'Bloque 3'){
+                if (horario.bloque === 'Bloque 3') {
                     console.log('Bloque 3')
-                    if((hora >=10 && minutos >= 50) || (hora <=11 && minutos <= 50)){
+                    if ((hora >= 10 && minutos >= 50) || (hora <= 11 && minutos <= 50)) {
                         console.log('Dentro del bloque 3')
                         setBloqueActual("Bloque 3")
                         setEstado(true);
                     }
-                    
+
                 }
-                if(horario.bloque === 'Bloque 4'){
+                if (horario.bloque === 'Bloque 4') {
                     console.log('Bloque 4')
-                    if((hora >=12 && minutos) >= 0 || (hora <=13 && minutos <= 0)){
+                    if ((hora >= 12 && minutos) >= 0 || (hora <= 13 && minutos <= 0)) {
                         console.log('Dentro del bloque 4')
                         setBloqueActual("Bloque 4")
                         setEstado(true);
                     }
                 }
-                if(horario.bloque === 'Bloque 5'){
+                if (horario.bloque === 'Bloque 5') {
                     console.log('Bloque 5')
-                    if((hora >=13 && minutos >= 10) || (hora <=14 && minutos <= 10) ){
+                    if ((hora >= 13 && minutos >= 10) || (hora <= 14 && minutos <= 10)) {
                         console.log('Entre en bloque 5')
                         setBloqueActual("Bloque 5")
                         setEstado(true);
-                    }    
+                    }
                 }
-                if(horario.bloque === 'Bloque 6'){
+                if (horario.bloque === 'Bloque 6') {
                     console.log('Bloque 6')
-                    if( (hora >=14 && minutos >= 20) || (hora <=15 && minutos <= 20) ){
+                    if ((hora >= 14 && minutos >= 20) || (hora <= 15 && minutos <= 20)) {
                         console.log('bloque 6 rdy')
                         setBloqueActual("Bloque 6")
                         setEstado(true);
                     }
                 }
-                if(horario.bloque === 'Bloque 7'){
+                if (horario.bloque === 'Bloque 7') {
                     console.log('bloque 7')
-                    if((hora >=15 && minutos >= 30) || (hora <=16 && minutos <= 30)){
+                    if ((hora >= 15 && minutos >= 30) || (hora <= 16 && minutos <= 30)) {
                         console.log('Dentro del bloque 7')
                         setBloqueActual("Bloque 7")
                         setEstado(true);
                     }
                 }
-                if(horario.bloque === 'Bloque 8'){
+                if (horario.bloque === 'Bloque 8') {
                     console.log('bloque 8');
-                    if((hora >=16 && minutos >= 40) || (hora <=17 && minutos <= 40)){
+                    if ((hora >= 16 && minutos >= 40) || (hora <= 17 && minutos <= 40)) {
                         console.log('Dentro del bloque 8');
                         setBloqueActual("Bloque 8")
                         setEstado(true);
                     }
                 }
-                if(horario.bloque === 'Bloque 9'){
+                if (horario.bloque === 'Bloque 9') {
                     console.log('Bloque')
-                    if( (hora >=17 && minutos >= 50) || (hora <=18 && minutos <= 50)){
+                    if ((hora >= 17 && minutos >= 50) || (hora <= 18 && minutos <= 50)) {
                         console.log('bloque 9');
                         setBloqueActual("Bloque 9")
                         setEstado(true);
                     }
                 }
-                if(horario.bloque === 'Bloque 10'){
+                if (horario.bloque === 'Bloque 10') {
                     console.log('bloque 10')
-                    if((hora >=19 && minutos >= 0) || (hora <=20 && minutos <= 0)){
+                    if ((hora >= 19 && minutos >= 0) || (hora <= 20 && minutos <= 0)) {
                         console.log('Dentro del bloque 10');
                         setBloqueActual("Bloque 10")
                         setEstado(true);
                     }
                 }
-                if(horario.bloque === 'Bloque 11'){
+                if (horario.bloque === 'Bloque 11') {
                     console.log('bloque 11');
-                    if((hora >=20 && minutos >= 10) || (hora <=21 && minutos <= 10)){
+                    if ((hora >= 20 && minutos >= 10) || (hora <= 21 && minutos <= 10)) {
                         setBloqueActual("Bloque 11")
                         console.log('Dentro del bloque 11');
                         setEstado(true);
@@ -165,21 +159,21 @@ const ListaAsistencia = ({estudiantes, modulo, id}) => {
         })
     }
 
-    const ActivarAsistencia =  () => {
+    const ActivarAsistencia = () => {
         setIniciarAsistencia(!iniciarAsistencia);
         let moduloNuevo = modulo;
-        
-        modulo.horario.map((horario ,index) => {
-            if(horario.bloque === bloqueActual){
-                if(moduloNuevo.horario[index].activo){
+
+        modulo.horario.forEach((horario, index) => {
+            if (horario.bloque === bloqueActual) {
+                if (moduloNuevo.horario[index].activo) {
                     moduloNuevo.horario[index].activo = false;
-                    getDatos();                    
-                }else{
+                    getDatos();
+                } else {
                     moduloNuevo.horario[index].activo = true;
-                    crearSubcoleccionAsistentes();                                    
+                    crearSubcoleccionAsistentes();
                 }
             }
-        })  
+        })
         updateDoc(doc(db, "modulos", id), moduloNuevo).then(() => {
 
         }).catch(() => {
@@ -188,62 +182,39 @@ const ListaAsistencia = ({estudiantes, modulo, id}) => {
     }
 
     const getDatos = () => {
-        let idNueva2 = 0;
-        let asisNueva2 ={
-            Activo:false,
-            asistentes:[],
-            fecha:"23/05/2022"
-        } ;
-        const q = query(collection(db, "modulos/"+id+"/asistencias"), where("Activo", "==", true));
+        const q = query(collection(db, "modulos/" + id + "/asistencias"), where("Activo", "==", true));
         getDocs(q).then((querySnapshot) => {
 
             let idNueva2 = 0;
-            let asisNueva2 ={
-                Activo:false,
-                asistentes:[],
-                fecha:"23/05/2022"
-            } ;
+            let asisNueva2 = {
+                Activo: false,
+                asistentes: [],
+                fecha: "23/05/2022"
+            };
             querySnapshot.forEach((doc) => {
                 idNueva2 = doc.id;
                 asisNueva2 = doc.data();
-                
-                /*asisNueva2.Activo = false;
-                console.log(asisNueva2);
-
-                updateDoc(doc(db, "modulos/kCs8O8wtNblwz4KMhEkn/asistencias/"+"m5M8Oj3vFfD3Xt6ObPqz"), asisNueva2).then(() =>{
-            
-                }).catch(() => {
-        
-                });   */ 
             });
             asisNueva2.Activo = false;
 
-            updateDoc(doc(db, "modulos/"+id+"/asistencias/"+idNueva2), asisNueva2).then(() =>{
-                
+            updateDoc(doc(db, "modulos/" + id + "/asistencias/" + idNueva2), asisNueva2).then(() => {
+
             }).catch(() => {
 
-            });  
+            });
         });
-        
-         //terminarAsistencia(idNueva2,asisNueva2);
-            
-    }
 
-    const terminarAsistencia = (idNuevo, asisNueva2) => {
-        let asisAux = asisNueva2;
-        asisAux.Activo = false;
-         
     }
-
-    const crearSubcoleccionAsistentes = async() => {
-        const fechaHoy = fechaActual.diaSemana + " " +fechaActual.mes + " " + fechaActual.anio;
+    const crearSubcoleccionAsistentes = async () => {
+        const fechaActualAux = new Date();
+        const fechaHoy = fechaActualAux.getDate() + "/" + (fechaActualAux.getMonth() + 1) + "/" + fechaActualAux.getFullYear();
         const alumnosPresentes = [];
-        await addDoc(collection(db, "modulos/"+id+"/asistencias"), {
-            Fecha : fechaHoy,
-            Asistentes : alumnosPresentes,
-            Activo : true
+        await addDoc(collection(db, "modulos/" + id + "/asistencias"), {
+            Fecha: fechaHoy,
+            Asistentes: alumnosPresentes,
+            Activo: true
         });
-        
+
     }
 
 
@@ -261,33 +232,33 @@ const ListaAsistencia = ({estudiantes, modulo, id}) => {
             <Grid item xs={12} md={12}>
                 {
                     estado ?
-                    (
-                        iniciarAsistencia ?
-                        (<Button
-                            variant='contained'
-                            onClick={ ActivarAsistencia}
-                        >
-                            Finalizar asistencia
-                        </Button>)
+                        (
+                            iniciarAsistencia ?
+                                (<Button
+                                    variant='contained'
+                                    onClick={ActivarAsistencia}
+                                >
+                                    Finalizar asistencia
+                                </Button>)
+                                :
+                                (<Button
+                                    variant='contained'
+                                    onClick={ActivarAsistencia}
+                                >
+                                    Tomar asistencia
+                                </Button>)
+                        )
                         :
-                        (<Button
-                            variant='contained'
-                            onClick={ ActivarAsistencia}
-                        >
-                            Tomar asistencia
-                        </Button>)
-                    )
-                    :
-                    (
-                        <Button 
-                            onClick={()=>{console.log(estado)}}
-                            disabled
-                        >
-                            Tomar asistencia
-                        </Button>
-                    )
+                        (
+                            <Button
+                                onClick={() => { console.log(estado) }}
+                                disabled
+                            >
+                                Tomar asistencia
+                            </Button>
+                        )
                 }
-                
+
                 {/* {
                     iniciarAsistencia ?
                     (<Button
@@ -305,12 +276,12 @@ const ListaAsistencia = ({estudiantes, modulo, id}) => {
                     </Button>)
                 } */}
 
-                </Grid>
+            </Grid>
 
-                <Grid item xs={12} md={12} style={{ marginTop: "10px" }}>
+            <Grid item xs={12} md={12} style={{ marginTop: "10px" }}>
                 {/* <ListaCursos estudiantes={estudiantes} /> */}
                 {
-                    estudiantes.map((estudiante, index)=>{
+                    /*estudiantes.map((estudiante, index)=>{
                         return (
                             <ListItem key={"listaEstudiantes_" + index}>
                                 <ListItemIcon >
@@ -331,7 +302,14 @@ const ListaAsistencia = ({estudiantes, modulo, id}) => {
 
                             </ListItem>
                         )
-                    })
+                    })*/
+                }
+                {
+                    <TablaAsistenciaModulo
+                        idModulo={id}
+                        iniciarAsistencia={iniciarAsistencia}
+                        estudiantesModulo={estudiantes}
+                    />
                 }
             </Grid>
         </>
---
+++ asistencia/src/Componentes/TablaAsistenciaModulo.js
@@ -0,0 +1,114 @@
+import React, { useEffect, useState } from 'react';
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Paper,
+    Skeleton,
+    IconButton
+} from '@mui/material/';
+import { db } from "../Utils/firebase";
+import { collection, query, onSnapshot } from "firebase/firestore";
+import RemoveRedEye from "@mui/icons-material/RemoveRedEye"
+import ModalListaAsistencia from './ModalListaAsistencia';
+export default function TablaAsistenciaModulo(props) {
+    const { idModulo, iniciarAsistencia, estudiantesModulo } = props;
+
+    const [asistencia, setAsistencias] = useState([])
+    const [cargandoDatos, setCargandoDatos] = useState(false)
+    const [mostrarAsistenciaModulo, setMostrarAsistenciaModulo] = useState(false)
+    const [asistenciaSeleccionada, setAsistenciaSeleccionada] = useState(null)
+
+
+    const q = query(collection(db, "modulos/" + idModulo + "/asistencias"));
+
+    useEffect(() => {
+        onSnapshot(q, (querySnapshot) => {
+            const asistenciasAux = []
+            querySnapshot.forEach((doc) => {
+                asistenciasAux.push({ id: doc.id, ...doc.data() })
+            });
+            setAsistencias(asistenciasAux)
+            setCargandoDatos(true)
+        });
+
+    }, [q])
+
+
+
+
+    function sortFunction(a, b) {
+        const fechaA = String(a.Fecha).split("/");
+        const fechaB = String(b.Fecha).split("/");
+        var dateA = new Date(fechaA[2] + "/" + fechaA[1] + "" / +fechaA[0]).getTime();
+        var dateB = new Date(fechaB[2] + "/" + fechaB[1] + "" / +fechaB[0]).getTime();
+        return dateA > dateB ? 1 : -1;
+    };
+    return (
+        <>
+            {
+                !cargandoDatos ? (
+                    <Skeleton variant="rectangular" width={210} height={118} />
+
+                ) : (
+                    <TableContainer component={Paper}>
+                        <Table >
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Fecha</TableCell>
+                                    <TableCell align="right">Estado</TableCell>
+                                    <TableCell align="right"></TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {asistencia.sort(sortFunction).map((row, idx) => (
+                                    <TableRow
+                                        key={"asistencia_" + idx}
+                                    >
+                                        <TableCell component="th" scope="row">
+                                            {row.Fecha}
+                                        </TableCell>
+                                        <TableCell align="right">{row.Activo ? "Activo" : "Inactivo"}</TableCell>
+                                        <TableCell align="right">
+                                            <IconButton
+                                                onClick={() => {
+                                                    setAsistenciaSeleccionada(row)
+                                                    setMostrarAsistenciaModulo(true);
+                                                }}
+                                            >
+                                                <RemoveRedEye />
+                                            </IconButton>
+
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+
+                            </TableBody>
+                        </Table >
+                    </TableContainer >
+
+                )
+            }
+
+            {mostrarAsistenciaModulo && (
+                <ModalListaAsistencia
+                    open={mostrarAsistenciaModulo}
+                    setOpen={setMostrarAsistenciaModulo}
+                    asistencia={asistenciaSeleccionada}
+                    idModulo={idModulo}
+                    iniciarAsistencia={iniciarAsistencia}
+                    estudiantesModulo={estudiantesModulo}
+                />
+
+
+            )}
+
+
+        </>
+
+
+    );
+}
