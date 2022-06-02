3d0d38e4b0514e54375f5243dc3cf4d8afec1372
Felipe <felipe.ortiz.san@gmail.com>
2022-06-02 17:58:25 -0400

    Historial de modulos muestra los modulos en los que aparece el usuario en la base de datos

--- asistencia/src/Paginas/Historial.js
+++ asistencia/src/Paginas/Historial.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { useAuth } from '../Context/AuthContext';
 import {
     Button,
     Checkbox,
@@ -20,11 +21,41 @@ import {
 }
     from '@mui/material';
 import NavbarInicio from '../Componentes/NavbarInicio';
+import { db } from '../Utils/firebase';
+import { collection, query, where, getDocs } from "firebase/firestore";
 
 
 
 export default function Historial(){
-  const lista = [1,2,3,4,5,6];
+
+  const { currentUser } = useAuth();
+
+  const [modulos, setModulos] = useState([])
+
+  useEffect(() => {
+    /* console.log(currentUser.email) */
+    async function obtenerModulos() {
+
+        const q = query(collection(db, "modulos"));
+        const querySnapshot = await getDocs(q);
+        const modulosAux = [];
+        querySnapshot.forEach((doc) => {
+          doc.data().estudiantes.map((estudiante) => {
+            if(estudiante.correo == currentUser.email){
+              /* console.log(estudiante.nombre+' está en la lista del modulo '+doc.data().nombre); */
+              modulosAux.push(doc.data());
+            }else{
+              /* console.log('no está en el modulo '+doc.data().nombre) */
+            }
+            
+          })
+        });
+        setModulos(modulosAux);
+      }
+      obtenerModulos()
+
+  }, [currentUser])
+
   return(
     <div>
       <NavbarInicio navBarActivo="Modulos" />
@@ -37,13 +68,13 @@ export default function Historial(){
 
 
          <Grid container spacing = {{xs : 2}} columns = {{xs: 4, sm: 8, md: 12}} marginTop = "5%" justifyContent = "center" >
-         {lista.map((index) => (
+         {modulos.map((modulo, index) => (
            <Grid item xs={2} sm={4} md={4} key={index}>
                <Card sx={{ height: 300 }}>
                  <CardContent>
                  <Box sx={{height: 250}}>
                    <Typography style= {{textAlign: 'center'}} >
-                     Modulo
+                     {modulo.nombre}
                    </Typography>
                  </Box>
 
--- asistencia/src/Paginas/Inicio.js
+++ asistencia/src/Paginas/Inicio.js
@@ -167,7 +167,6 @@ export default function Inicio() {
                                             <Button
                                                 fullWidth
                                                 variant='contained'
-
                                             >
                                                 Historial de Asistencia
 
